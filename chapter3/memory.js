3ba1116b250a228b852852a43d64dfb84fa98b5b
Shikhar Mathur <totalboron@gmail.com>
2020-07-07 17:02:37 +0530

    all files arranged accoding to chapters

--- chapter3/memory.js
+++ chapter3/memory.js
@@ -5,4 +5,4 @@ console.log("Total Memory=", os.totalmem * gigaByte, "GB");
 console.log("Available Memory=", os.freemem * gigaByte, "GB");
 console.log("Memory Used=", (os.totalmem - os.freemem) * gigaByte);
 
-console.log(os.cpus().length);
+console.log("No of cpu threads", os.cpus().length);
